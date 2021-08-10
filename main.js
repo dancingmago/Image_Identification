4f3cc9c61216c0dea0363c4374d3a9605123200b
dancingmago <73450506+dancingmago@users.noreply.github.com>
2021-08-09 18:47:10 -0700

    .

---
+++ main.js
@@ -0,0 +1,39 @@
+Webcam.set({
+    width: 310,
+    height: 300,
+    image_format: "png",
+    png_quality: 90,
+
+    constraints: {
+        facingMode: "enviornment"
+    }
+});
+camera = document.getElementById("camera");
+Webcam.attach("#camera");
+
+function take_snapshot(){
+    Webcam.snap(function (data_uri){
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+ data_uri +'"/>';
+    });
+}
+console.log("ml5.version", ml5.version);
+Classifier = ml5.imageClassifier("MobileNet", modelLoaded);
+
+function modelLoaded(){
+    console.log("Model Loaded");
+}
+
+function check(){
+    img = document.getElementById("captured_image");
+    Classifier.classify(img, got_result);
+}
+
+function got_result(error, results){
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        document.getElementById("object_name").innerHTML = results[0].label;
+    }
+}
